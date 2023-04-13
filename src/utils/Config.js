6561861556b1e123fd873b6234310dcec29ce2a7
kevoecodes <kelvinremmy01@gmail.com>
2023-04-13 19:01:36 +0300

    Commit

---
+++ src/utils/Config.js
@@ -0,0 +1,10 @@
+export const Config = {
+    apiURL: "http://127.0.0.1:7700/",
+    socketURL: "ws://127.0.0.1:7700/",
+    mediaURL: "http://127.0.0.1:4000/streamer/v1/",
+    // apiURL: 'https://www.foleniqms.com/',
+    // socketURL: "wss://www.foleniqms.com/",
+    // mediaURL: 'https://www.foleniqms.com/streamer/v1/',
+    ideal_width: 1340,
+    ideal_height: 680
+}
